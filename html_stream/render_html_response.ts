cd664242bca70bbe7d59714dc63da7c545ea8eac
Mark Gibson <mgibson@adaptavist.com>
2024-12-18 11:03:49 +0000

    feat: response-node-cache

--- html_stream/render_html_response.ts
+++ html_stream/render_html_response.ts
@@ -3,6 +3,35 @@ import type { RenderOptions } from "@http/token-stream/types";
 import { renderHtmlBody } from "./render_html_body.ts";
 
 /**
+ * Options for `renderHtmlResponse` function
+ */
+export type RenderHtmlResponseOptions = RenderOptions<HtmlToken> & {
+  /**
+   * Additional headers to pass in the Response
+   */
+  headers?: HeadersInit;
+
+  /**
+   * A function that caches the given HtmlNode against the created
+   * Response.
+   *
+   * @example
+   * ```tsx
+   * import { renderHtmlResponse } from "@http/html-stream/render-html-response";
+   * import { cacheHtmlNode, getHtmlNode } from "@http/html-stream/response-node-cache";
+   *
+   * const response = renderHtmlResponse(<Hello/>, { cacheHtmlNode });
+   *
+   * const htmlNode = getHtmlNode(response);
+   * ```
+   *
+   * @param response
+   * @param node
+   */
+  cacheHtmlNode?: (response: Response, node: HtmlNode) => unknown;
+};
+
+/**
  * Render the given node as a HTML {@linkcode Response} with a prepended DOCTYPE.
  *
  * @param node the HTML node
@@ -11,15 +40,19 @@ import { renderHtmlBody } from "./render_html_body.ts";
  */
 export function renderHtmlResponse(
   node: HtmlNode,
-  options?: RenderOptions<HtmlToken> & { headers?: HeadersInit },
+  options?: RenderHtmlResponseOptions,
 ): Response {
   const headers = new Headers(options?.headers);
   if (!headers.has("Content-Type")) {
-    headers.set("Content-Type", "text/html");
+    headers.set("Content-Type", "text/html; charset=utf-8");
   }
-  return new Response(renderHtmlBody(node, options), {
+  const response = new Response(renderHtmlBody(node, options), {
     status: 200,
     statusText: "OK",
     headers,
   });
+
+  options?.cacheHtmlNode?.(response, node);
+
+  return response;
 }
---
+++ html_stream/response_node_cache.ts
@@ -0,0 +1,39 @@
+import type { HtmlNode } from "@http/html-stream/types";
+
+const cache = new WeakMap<Response, HtmlNode>();
+
+/**
+ * Cache the original HtmlNode against the Response in which it will be delivered.
+ * This can be used to avoid having to parse the Response of Request handler when
+ * called internally.
+ *
+ * @param response the Response that serializes the HtmlNode
+ * @param node the original HtmlNode represented by the Response
+ * @returns the Response passed in
+ */
+export function cacheHtmlNode(response: Response, node: HtmlNode): Response {
+  cache.set(response, node);
+  return response;
+}
+
+/**
+ * Get the cached HtmlNode for a Response.
+ *
+ * @param response
+ * @returns a HtmlNode if found
+ */
+export function getHtmlNode(response: Response): HtmlNode | undefined {
+  return cache.get(response);
+}
+
+/**
+ * Explicitly delete any cached HtmlNode associated with a Response.
+ * This isn't strictly necessary, as a WeakMap is used for the cache,
+ * so the HtmlNode should be automatically cleaned up when its Response
+ * is garbage collected.
+ *
+ * @param response a Response that may have an associated cached HtmlNode
+ */
+export function invalidateHtmlNode(response: Response): void {
+  cache.delete(response);
+}
